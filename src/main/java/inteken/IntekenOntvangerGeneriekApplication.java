1f583010073f0e37a73e3a574e2a2a5e2cb5e04e
Okke Harsta <oharsta@zilverline.com>
2020-11-18 14:28:49 +0100

    Added spring app

---
+++ src/main/java/inteken/IntekenOntvangerGeneriekApplication.java
@@ -0,0 +1,13 @@
+package inteken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntekenOntvangerGeneriekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntekenOntvangerGeneriekApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inteken/IntekenOntvangerGeneriekApplicationTests.java
@@ -0,0 +1,13 @@
+package inteken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntekenOntvangerGeneriekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
